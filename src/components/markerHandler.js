6170e8ac40aefc2b442cc781964e2fafa2454e02
Aenoris <aenoriss@gmail.com>
2022-04-25 07:32:40 -0300

    asda

--- src/components/markerHandler.js
+++ src/components/markerHandler.js
@@ -23,7 +23,7 @@ AFRAME.registerComponent("markerhandler", {
           status.trigger.forEach((e => {
             if(e.id == elemId){
               triggerSelected = e;
-              alert(triggerSelected);
+              console.log("triggerSelected", triggerSelected)
             }
           }))
 
