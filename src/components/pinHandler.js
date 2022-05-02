3d9f5461dfe1f531fe755b844f6dbb831e52b88b
Aenoris <aenoriss@gmail.com>
2022-05-02 04:27:06 -0300

    xzxczxc

--- src/components/pinHandler.js
+++ src/components/pinHandler.js
@@ -11,11 +11,11 @@ AFRAME.registerComponent("pinhandler", {
 
     console.log("PIN HANDLER ACTIVATED")
 
-    let marker0 = document.getElementById("pin0");
-    let marker1 = document.getElementById("pin1");
-    let marker2 = document.getElementById("pin2");
-    let marker3 = document.getElementById("pin3");
-    let marker4 = document.getElementById("pin4");
+    let marker0 = document.getElementById("marker0");
+    let marker1 = document.getElementById("marker1");
+    let marker2 = document.getElementById("marker2");
+    let marker3 = document.getElementById("marker3");
+    let marker4 = document.getElementById("marker4");
 
     socket.on("eggOverview", async (data) => {
       console.log("DATAA TIC", data)
@@ -32,25 +32,25 @@ AFRAME.registerComponent("pinhandler", {
             break;
           case 2:
             if(marker.taken == true){
-              console.log("marker elem", marker0)
+              console.log("marker elem", marker1)
               marker1.setAttribute("visible", false);
             } 
             break;
           case 3:
             if(marker.taken == true){
-              console.log("marker elem", marker0)
+              console.log("marker elem", marker2)
               marker2.setAttribute("visible", false);
             } 
             break;
           case 4:
             if(marker.taken == true){
-              console.log("marker elem", marker0)
+              console.log("marker elem", marker3)
               marker3.setAttribute("visible", false);
             } 
             break;
           case 5:
             if(marker.taken == true){
-              console.log("marker elem", marker0)
+              console.log("marker elem", marker4)
               marker4.setAttribute("visible", false);
             } 
             break;
